74023afab0f7e82c30c905cf854875ad5a4a3ef3
Bognar Junior <bognar_junior@yahoo.com.br>
2019-04-05 19:20:26 -0300

    Create toggle component
    Criando o componente de Toggle

---
+++ components/Toggle.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export default class Toggle extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+				<View style={[styles.viewNot, styles.viewToggle]}>
+					<Text style={styles.activeLabel}> Não </Text>
+				</View>
+				<View style={[styles.viewYes, styles.viewToggle]}>
+					<Text style={styles.activeLabel}> Sim </Text>
+				</View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		borderWidth: 1,
+    backgroundColor: '#d1d1e0',
+	},
+  activeLabel: {
+    color: '#ffffff'
+	},
+  viewYes: {
+		backgroundColor: '#008000',
+	},
+  viewNot: {
+		backgroundColor: '#ff0055',
+	},
+	viewToggle: {
+		padding: 4,
+	}
+})
